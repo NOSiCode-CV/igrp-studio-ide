8ad6506c12d5a29659d1f499d72cef90dfdc025c
Carlos Graca <carlosa.graca@nosi.cv>
2024-11-27 03:28:24 +0000

    Recents slice 3

--- src/renderer/src/pages/home/components/RecentsProjects.tsx
+++ src/renderer/src/pages/home/components/RecentsProjects.tsx
@@ -172,7 +172,7 @@ const RecentsProjects = (): JSX.Element => {
             <LoadingSpinner />
           ) : allProjects.data.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
-              {allProjects.data.map((project) => renderProjectCard(project, true))}
+              {allProjects.data.slice(0, 3).map((project) => renderProjectCard(project, true))}
             </div>
           ) : (
             <EmptyState
