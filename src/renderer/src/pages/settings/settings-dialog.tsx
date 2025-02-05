436a8bd4e92a61d3c851ad031507fa1ddc8a2447
Carlos Graca <carlosa.graca@nosi.cv>
2025-02-05 17:02:36 -0100

    fixebug: Dialog settings

--- src/renderer/src/pages/settings/settings-dialog.tsx
+++ src/renderer/src/pages/settings/settings-dialog.tsx
@@ -13,6 +13,7 @@ import {
     Dialog,
     DialogContent,
     DialogDescription,
+    DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from '@renderer/components/ui/dialog';
@@ -29,10 +30,15 @@ import {
 import { AboutSettings } from './about-settings';
 import { LanguageSettings } from './language-settings';
 import { ConnectedAccountsSettings } from './connected-accounts-settings';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@renderer/components/ui/button';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@renderer/components/ui/tooltip';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from '@renderer/components/ui/tooltip';
+import { ScrollArea } from '@renderer/components/ui/scroll-area';
 
 const data = {
     nav: [
@@ -65,13 +71,15 @@ export function SettingsDialog() {
                 </TooltipTrigger>
                 <TooltipContent>{t('settings')}</TooltipContent>
             </Tooltip>
-
             <DialogContent className="overflow-hidden p-0 md:max-h-[500px] md:max-w-[800px] max-w-[900px]">
-                <DialogTitle className="sr-only">{t('settings')}</DialogTitle>
-                <DialogDescription className="sr-only">
-                    Customize your settings here.
-                </DialogDescription>
-
+                <DialogHeader className='pb-3'>
+                    <DialogTitle className="sr-only">
+                        {t('settings')}
+                    </DialogTitle>
+                    <DialogDescription className="sr-only">
+                        Customize your settings here.
+                    </DialogDescription>
+                </DialogHeader>
                 <SidebarProvider className="items-start">
                     <Sidebar collapsible="none" className="hidden md:flex">
                         <SidebarContent>
@@ -82,12 +90,18 @@ export function SettingsDialog() {
                                             <SidebarMenuItem key={item.name}>
                                                 <SidebarMenuButton
                                                     asChild
-                                                    isActive={item.name === activeItem}
-                                                    onClick={() => setActiveItem(item.name)}
+                                                    isActive={
+                                                        item.name === activeItem
+                                                    }
+                                                    onClick={() =>
+                                                        setActiveItem(item.name)
+                                                    }
                                                 >
                                                     <button>
                                                         <item.icon />
-                                                        <span>{t(item.name)}</span>
+                                                        <span>
+                                                            {t(item.name)}
+                                                        </span>
                                                     </button>
                                                 </SidebarMenuButton>
                                             </SidebarMenuItem>
@@ -97,39 +111,46 @@ export function SettingsDialog() {
                             </SidebarGroup>
                         </SidebarContent>
                     </Sidebar>
-                    <main className="flex h-[480px] flex-1 flex-col overflow-hidden">
+
+                    <main className="flex h-[480px] flex-1 flex-col">
                         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                             <div className="flex items-center gap-2 px-4">
                                 <Breadcrumb>
                                     <BreadcrumbList>
                                         <BreadcrumbItem className="hidden md:block">
                                             <BreadcrumbLink href="#/">
-                                                Settings
+                                                {t('settings')}
                                             </BreadcrumbLink>
                                         </BreadcrumbItem>
                                         <BreadcrumbSeparator className="hidden md:block" />
                                         <BreadcrumbItem>
                                             <BreadcrumbPage>
-                                                {activeItem}
+                                                {t(activeItem)}
                                             </BreadcrumbPage>
                                         </BreadcrumbItem>
                                     </BreadcrumbList>
                                 </Breadcrumb>
                             </div>
                         </header>
-                        <div className="flex flex-1 flex-col gap-4 overflow-y-auto p-4 pt-0">
-                            {data.nav.find((item) => item.name === activeItem)?.component ? (
-                                React.createElement(
-                                    data.nav.find((item) => item.name === activeItem)!.component!
-                                )
-                            ) : (
-                                <div className="flex items-center justify-center h-full">
-                                    <p className="text-muted-foreground">
-                                        Select a setting to view
-                                    </p>
-                                </div>
-                            )}
-                        </div>
+                        <ScrollArea>
+                            <div className="flex flex-1 flex-col gap-4 overflow-y-auto p-4 pt-0">
+                                {data.nav.find(
+                                    (item) => item.name === activeItem
+                                )?.component ? (
+                                    React.createElement(
+                                        data.nav.find(
+                                            (item) => item.name === activeItem
+                                        )!.component!
+                                    )
+                                ) : (
+                                    <div className="flex items-center justify-center h-full">
+                                        <p className="text-muted-foreground">
+                                            Select a setting to view
+                                        </p>
+                                    </div>
+                                )}
+                            </div>
+                        </ScrollArea>
                     </main>
                 </SidebarProvider>
             </DialogContent>
