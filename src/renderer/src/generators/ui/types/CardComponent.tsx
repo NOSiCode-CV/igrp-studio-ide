ef87147ff7173ff60bd9615bd08e9fad47f91f35
Carlos Graca <carlosa.graca@nosi.cv>
2025-11-22 00:15:36 -0100

    refactor: update component imports and mappings in ComponentTypes.ts and CardComponent.tsx, replacing IGRPDatePicker with IGRPDatePickerSingle and IGRPLabelPrimitive with IGRPLabel

--- src/renderer/src/generators/ui/types/CardComponent.tsx
+++ src/renderer/src/generators/ui/types/CardComponent.tsx
@@ -56,9 +56,6 @@ const CardComponent = ({ comp, group, hoverClass, onDragEnd }: CardComponentProp
         >
           {content || FAKE_COMPONENT_DATA?.properties?.content}
 
-          {/*   {componentName === COMPONENT.Label && label && (
-            <div className="text-sm font-medium text-muted-foreground truncate">{label}</div>
-          )} */}
         </Component>
       ) : (
         <div className="rounded-lg shadow-xs border p-4 bg-card">
