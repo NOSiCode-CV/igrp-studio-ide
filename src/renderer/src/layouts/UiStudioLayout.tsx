99c1b1b02790daab5779748c3ddac9a8555bb423
Carlos Graca <carlosa.graca@nosi.cv>
2025-12-19 11:00:58 -0100

    chore: update package versions in package.json and pnpm-lock.yaml; bump @igrp/igrp-studio-nextjs-engine to 0.1.0-beta.57, @reduxjs/toolkit to 2.11.2, @tailwindcss/vite to 4.1.18, eslint-plugin-react-refresh to 0.4.25, and various other dependencies

--- src/renderer/src/layouts/UiStudioLayout.tsx
+++ src/renderer/src/layouts/UiStudioLayout.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './components/header'
 import { createSelector } from 'reselect'
 import { useSelector } from 'react-redux'
 import { IGRPSidebarProvider } from '@igrp/igrp-framework-react-design-system'
 import { Footer } from './components/footer'
+import { ROUTES } from '@renderer/routes/routeConstants'
+import { useNavigate } from 'react-router-dom'
 
 interface LayoutProps {
   children: React.ReactElement<{ basePath: string }>
@@ -23,7 +25,15 @@ const Layout = (props: LayoutProps): React.JSX.Element => {
     basePath: studio.basePath
   }))
 
+  const navigate = useNavigate()
+
   const { config, basePath } = useSelector(selectStudioProperties)
+
+  useEffect(() => {
+    if (!basePath) {
+      navigate(ROUTES.IDE_INITIAL_SCREEN, { replace: true })
+    }
+  }, [navigate, basePath])
 
   return (
     <div className="[--header-height:calc(--spacing(10))] [--header-height-two:calc(--spacing(20))] [--header-height-three:calc(--spacing(28))]">
